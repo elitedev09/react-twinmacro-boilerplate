10e67ca176ec83a0304baf6630672075a28c7953
piaocheng0811 <pc61300811@gmail.com>
2021-09-13 18:13:04 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+/** @jsxImportSource @emotion/react */
+import "twin.macro";
+
+function App() {
+  return (
+    <div tw="max-w-4xl mx-auto  mt-5">
+      <h1 tw="text-purple-700 text-opacity-50 text-5xl">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
